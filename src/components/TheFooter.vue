9b6610bd4a3bab31ca0c0112e2bd62dde167204c
Brian Zelip <bzelip@gmail.com>
2019-06-10 15:11:15 -0400

    TheFooter.vue: Create file from hardcoded footer in App.vue

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,7 @@
+<template lang="pug">
+  footer
+    a(href="https://www.zelip.me") by bzelip
+    span &nbsp; | &nbsp;
+    a(href="https://github.com/brianzelip/biohackathons.org") Source code
+  </footer>
+</template>
