4f9660f13ebe4b1f2f4ca9a058c43556814733fa
Brian Zelip <bzelip@gmail.com>
2019-06-12 11:12:33 -0400

    HackathonsList: Import yaml as json via Parcel, render out data!

--- src/components/HackathonsList.vue
+++ src/components/HackathonsList.vue
@@ -1,15 +1,17 @@
 <template lang="pug">
   section.vh100
+    ol
+      li(v-for="hackathon in hackathons") {{ hackathon.date }}
 </template>
 
 <script>
+import hackathonsList from "../data/upcomingHackathons.yml";
+
 export default {
-  data() {
-    return {
-      eventList: {}
-    };
-  },
-  created() {},
-  mounted() {}
+  computed: {
+    hackathons() {
+      return hackathonsList;
+    }
+  }
 };
 </script>
