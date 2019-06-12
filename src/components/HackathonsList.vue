18ef853571d73a10f7c4cdcfb5ea88ffa7a20547
Brian Zelip <bzelip@gmail.com>
2019-06-12 08:32:06 -0400

    HackathonsList: Add broke js to read yaml
    Trying to read yaml in vue, but it needs node; need to look at
    parcel plugins for reading yaml

--- src/components/HackathonsList.vue
+++ src/components/HackathonsList.vue
@@ -1,3 +1,28 @@
 <template lang="pug">
   section.vh100
 </template>
+
+<script>
+const fs = require("fs");
+const path = require("path");
+import yaml from "js-yaml";
+
+export default {
+  data() {
+    return {
+      eventList: {}
+    };
+  },
+  created() {
+    // console.log("component created!");
+    // const data = yaml.safeLoad("../data/upcomingHackathons.yml");
+    // console.log("data obj is:::::", data);
+  },
+  mounted() {
+    const doc = fs.readFileSync(
+      path.join(__dirname, "../data/upcomingHackathons.yml"),
+      "UTF-8"
+    );
+  }
+};
+</script>
