94dbac38b5aac242c7dc9342cedb1b519125315d
Brian Zelip <bzelip@gmail.com>
2019-06-12 06:48:52 -0400

    App.vue: Register and render HackathonsList

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,16 +1,19 @@
 <template lang="pug">
   div.vh100.flex.flex-column.align-items-center
     TheHeader
+    HackathonsList
     TheFooter.flex-grow.flex.align-items-end.pb2
 </template>
 
 <script>
 import TheHeader from "./TheHeader.vue";
 import TheFooter from "./TheFooter.vue";
+import HackathonsList from "./HackathonsList.vue";
 
 export default {
   components: {
     TheHeader,
+    HackathonsList,
     TheFooter
   }
 };
