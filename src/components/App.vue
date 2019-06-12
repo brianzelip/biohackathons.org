5b9cb782001a6e1541d6b7050fa34859b6ca24d7
Brian Zelip <bzelip@gmail.com>
2019-06-12 16:04:58 -0400

    App.vue: Refactor around sending yaml data as props to a reusable component

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,11 +1,15 @@
 <template lang="pug">
   div.vh100.flex.flex-column.align-items-center
     TheHeader
-    HackathonsList
+    HackathonsList(listType="upcoming" :hackathons="upcomingHackathons")
+    HackathonsList(listType="past" :hackathons="pastHackathons")
     TheFooter.flex-grow.flex.align-items-end.pb2
 </template>
 
 <script>
+import upcomingHackathonsData from "../data/upcomingHackathons.yml";
+import pastHackathonsData from "../data/pastHackathons.yml";
+
 import TheHeader from "./TheHeader.vue";
 import TheFooter from "./TheFooter.vue";
 import HackathonsList from "./HackathonsList.vue";
@@ -15,6 +19,14 @@ export default {
     TheHeader,
     HackathonsList,
     TheFooter
+  },
+  computed: {
+    upcomingHackathons() {
+      return upcomingHackathonsData;
+    },
+    pastHackathons() {
+      return pastHackathonsData;
+    }
   }
 };
 </script>
