92ba0f6b9c6761a1d33cbb1e50b888349402cb5f
Brian Zelip <bzelip@gmail.com>
2019-06-25 08:17:17 -0400

    TheHomePage.vue: Create component for a router view

---
+++ src/components/TheHomePage.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  section
+    HackathonsList(listType="upcoming" :hackathons="upcomingHackathons")#events
+    HackathonsList(listType="past" :hackathons="pastHackathons")
+</template>
+
+<script>
+import upcomingHackathonsData from "../data/upcomingHackathons.yml";
+import pastHackathonsData from "../data/pastHackathons.yml";
+
+import HackathonsList from "./HackathonsList.vue";
+
+export default {
+  components: {
+    HackathonsList
+  },
+  computed: {
+    upcomingHackathons() {
+      return upcomingHackathonsData;
+    },
+    pastHackathons() {
+      return pastHackathonsData;
+    }
+  }
+};
+</script>
