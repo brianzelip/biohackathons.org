9900ed669e2a3e636fb79625c17e2e33cbab81d4
Brian Zelip <bzelip@gmail.com>
2019-06-25 08:18:09 -0400

    App.vue: Remove functionality refactored to TheHomePage

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,32 +1,18 @@
 <template lang="pug">
   div.vh100.flex.flex-column.align-items-center
     TheHeader
-    HackathonsList(listType="upcoming" :hackathons="upcomingHackathons")#events
-    HackathonsList(listType="past" :hackathons="pastHackathons")
+    router-view
     TheFooter.flex-grow.flex.align-items-end.pb2
 </template>
 
 <script>
-import upcomingHackathonsData from "../data/upcomingHackathons.yml";
-import pastHackathonsData from "../data/pastHackathons.yml";
-
 import TheHeader from "./TheHeader.vue";
 import TheFooter from "./TheFooter.vue";
-import HackathonsList from "./HackathonsList.vue";
 
 export default {
   components: {
     TheHeader,
-    HackathonsList,
     TheFooter
-  },
-  computed: {
-    upcomingHackathons() {
-      return upcomingHackathonsData;
-    },
-    pastHackathons() {
-      return pastHackathonsData;
-    }
   }
 };
 </script>
