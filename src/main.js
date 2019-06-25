b61218bb0ff473f42e66ad65b16d0ebf6eb14044
Brian Zelip <bzelip@gmail.com>
2019-06-25 08:18:35 -0400

    main.js: Import and use router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 
+import router from './router.js';
 import App from './components/App.vue';
+
+Vue.use(VueRouter);
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 });
