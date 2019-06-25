f03c6eb9395400f216605813521fd454dae9dff5
Brian Zelip <bzelip@gmail.com>
2019-06-25 14:22:52 -0400

    TheFooter: Add markup that resembles current site data

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,8 +1,19 @@
 <template lang="pug">
   footer.mt3.py3.col-12.bg-black
-    .container
-      a(href="https://www.zelip.me") by bzelip
-      span.white &nbsp; | &nbsp;
-      a(href="https://github.com/brianzelip/biohackathons.org") Source code
+    .container.flex.white
+      section.col-3
+        h1.h2.regular Contact
+        ul.list-reset
+          li
+            a(href="mailto:ben.busby@nih.gov") ben.busby@nih.gov
+          li
+            a(href="mailto:biohackathons@gmail.com") biohackathons@gmail.com
+          li
+            a(href="https://twitter.com/dcgenomics") @dcgenomics
+      section.col-3
+        h1.h2.regular Publicity
+        ul.list-reset
+          li
+            a(href="https://ncbiinsights.ncbi.nlm.nih.gov/tag/hackathon/") NCBI blog posts about hackathons
   </footer>
 </template>
