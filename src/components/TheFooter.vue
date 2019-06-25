bdaee8893597a2131bb52a22b74e66be26c2380e
Brian Zelip <bzelip@gmail.com>
2019-06-25 14:05:38 -0400

    TheFooter: Shift presentation styles to TheFooter from App.vue

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,7 +1,8 @@
 <template lang="pug">
-  footer
-    a(href="https://www.zelip.me") by bzelip
-    span &nbsp; | &nbsp;
-    a(href="https://github.com/brianzelip/biohackathons.org") Source code
+  footer.mt3.py3.col-12.bg-black
+    .container
+      a(href="https://www.zelip.me") by bzelip
+      span.white &nbsp; | &nbsp;
+      a(href="https://github.com/brianzelip/biohackathons.org") Source code
   </footer>
 </template>
